f3e1562192aff0a14d202f83bb57ca83031c05e4
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-23 21:14:09 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,10 @@
+output "table1_arn" {
+  description = "The ARN of table1
+  value = aws_dynamodb_table.example_table_1.name
+}
+
+output "table1_arn" {
+  description = "The ARN of table2
+  value = aws_dynamodb_table.example_table_2.name
+}
+
