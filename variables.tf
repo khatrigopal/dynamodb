683511edf69f2913fe7b636a49c8fad567ed0f9c
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-23 21:02:12 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,13 @@
 variable "table1_name" {
   description = "The name of the first DynamoDB table."
   type        = string
+  default = "tabel1"
 }
 
 variable "table2_name" {
   description = "The name of the second DynamoDB table."
   type        = string
+  default = "tabel2"
 }
 
 variable "billing_mode" {
@@ -17,19 +19,23 @@ variable "billing_mode" {
 variable "table1_hash_key" {
   description = "The name of the hash key for the first DynamoDB table."
   type        = string
+  default     = "jobid"
 }
 
 variable "table1_range_key" {
   description = "The name of the range key for the first DynamoDB table."
   type        = string
+  default = "datecreation"
 }
 
 variable "table2_hash_key" {
   description = "The name of the hash key for the second DynamoDB table."
   type        = string
+  default = "testid"
 }
 
 variable "table2_range_key" {
   description = "The name of the range key for the second DynamoDB table."
   type        = string
+  default = "creatondate"
 }
