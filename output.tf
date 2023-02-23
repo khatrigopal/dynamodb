1ef63a6a5c64f225408e9806ddd2c5b4a1a80b21
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-23 21:21:22 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -8,13 +8,23 @@ output "table2_arn" {
   value = aws_dynamodb_table.example_table_2.arn
 }
 
-output "table1_arn" {
+output "table1_name" {
   description = "The ARN of table1"
   value = aws_dynamodb_table.example_table_1.name
 }
 
-output "table2_arn" {
+output "table2_name" {
   description = "The ARN of table2"
   value = aws_dynamodb_table.example_table_2.name
+}
+
+output "table1_id" {
+  description = "The ARN of table1"
+  value = aws_dynamodb_table.example_table_1.id
+}
+
+output "table2_id" {
+  description = "The ARN of table2"
+  value = aws_dynamodb_table.example_table_2.id
 }
 
